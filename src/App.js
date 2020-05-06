add544f64455a5616ddd490e89395594194404e2
= <=>
2020-05-06 07:15:11 -0300

    Desenvolvimento de desafio

--- src/App.js
+++ src/App.js
@@ -60,7 +60,7 @@ export default function App() {
               <View style={styles.likesContainer}>
                 <Text
                   style={styles.likeText}
-                  testID={`repository-likes-${repository.id}`}
+                  testID={`repository-likes-${repository.id}`} 
                  >
                   {repository.likes} curtida{repository.likes > 1 ? 's' : ''}
                 </Text>
